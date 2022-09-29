0d90901a3d4a2a4a93f08a626612b91d663cf633
Babychandana <96607956+Babychandana@users.noreply.github.com>
2022-09-29 14:46:46 +0530

    first commit

---
+++ eclipse-workspace/SelenuimProject/src/main/java/pack/testt.java
@@ -0,0 +1,18 @@
+package pack;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class testt {
+
+	//public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	@Test
+	public void tes()
+	{
+		Reporter.log("bye",true);
+	}
+
+}
